27e5f424bb2f0b39120201cd3767adfdb8315976
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2021-12-09 21:32:12 -0300

    add verificações em edit

--- src/utils/functionsDB.js
+++ src/utils/functionsDB.js
@@ -10,57 +10,61 @@ import { data } from '../db.jsx';
 let res = false;
 let red = false;
 
-// create 
-export const create = async (dado) => {
-  let nome = dado.nome
-  let modelo = dado.model
-  let placa = dado.placa
-  let vaga = dado.vaga
-  let funcionario = dado.funcionario
 
-  return await verificaPlaca(nome, modelo, placa, vaga, funcionario);
-};
-
+// funções publicas
 const verificaPlaca =  async (nome, modelo, placa, vaga, funcionario) => {
-  let regexPlaca1 = /([A-Z]{3})([0-9]{4})/gi;
-  let regexPlaca2 = /([A-Z]{3})([0-9]{1})([A-Z]{1})([0-9]{2})/gi;
+  let resul1 = /([A-Z]{3})([0-9]{4})/gi.test(placa); /*padrão aaa1111 */
+  let resul2 = /([A-Z]{3})([0-9]{1})([A-Z]{1})([0-9]{2})/gi.test(placa); /*padrão aaa1a11 */
 
-  let resul1 = regexPlaca1.test(placa)
-  let resul2 = regexPlaca2.test(placa)
-
-  if (resul1) {
-    return await verificaGeral(nome, modelo, placa, vaga, funcionario);
-  } else if (resul2) {
-    return await verificaGeral(nome, modelo, placa, vaga, funcionario);
-  } else {
-    alert('Modelo de Placa INVALIDA!!!')
-  }
+  if(resul1) return "modelo-placa-1";
+  if(resul2) return "modelo-placa-2";
+  else return false;
 }
 
 const verificaGeral = async (nome, modelo, placa, vaga, funcionario) => {
-  res = false
-  if(
-    ((nome && modelo) !== '' || null)
-    && (vaga !== "Vaga") 
-    && (funcionario !== "Funcionarios")
-  ){
-    
-    data.push(
-      {
-        id: data.length + 1,
-        nome: nome, 
-        placa: placa, 
-        model: modelo,
-        vaga: Number(vaga),
-        funcionario: funcionario
-      },
-    )
-    res = true;
-  }else{
-    alert('Preencha todas as informações!!!!')
-  }
-  return(res);
+  if(((nome && modelo) !== '' || null) && (vaga !== "Vaga") && (funcionario !== "Funcionarios")) return true;
 }
+
+// create 
+export const create = async (dado) => {
+  res = false
+  let nome = dado.nome;
+  let modelo = dado.model;
+  let placa = dado.placa;
+  let vaga = dado.vaga;
+  let funcionario = dado.funcionario;
+
+  await verificaPlaca(nome, modelo, placa, vaga, funcionario)
+    .then((resposta) => {
+      if(resposta === "modelo-placa-1") {
+         verificaGeral(nome, modelo, placa, vaga, funcionario)
+          .then((res) => res ?  addBanco(nome, modelo, placa, vaga, funcionario) : alert('Preencha todas as informações!!!!'));
+      }
+
+      if(resposta === "modelo-placa-2") {
+         verificaGeral(nome, modelo, placa, vaga, funcionario)
+          .then((res) => res ?  addBanco(nome, modelo, placa, vaga, funcionario) : alert('Preencha todas as informações!!!!'));
+      }
+
+      if(resposta === false) alert("Modelo de Placa Invalido!!!");
+    });
+  return res;
+};
+
+const addBanco = async (nome, modelo, placa, vaga, funcionario) => {
+  data.push(
+    {
+      id: data.length + 1,
+      nome: nome, 
+      placa: placa, 
+      model: modelo,
+      vaga: Number(vaga),
+      funcionario: funcionario
+    },
+  );
+  res = true;
+}
+
 
 // =================================================================================================
 // delete 
@@ -75,11 +79,30 @@ export const deletar = async (props) => {
   return data
 }
 
+
 // =================================================================================================
 // update 
-
 export const update = async (dado) => {
   red = false;
+  await verificaPlaca(dado.nome, dado.model, dado.placa, dado.vaga, dado.funcionario)
+    .then((resposta) => {
+      if(resposta === "modelo-placa-1") {
+         verificaGeral(dado.nome, dado.model, dado.placa, dado.vaga, dado.funcionario)
+          .then((res) => res ?  updateBanco(dado) : alert('Preencha todas as informações!!!!'));
+      }
+
+      if(resposta === "modelo-placa-2") {
+         verificaGeral(dado.nome, dado.model, dado.placa, dado.vaga, dado.funcionario)
+          .then((res) => res ?  updateBanco(dado) : alert('Preencha todas as informações!!!!'));
+      }
+
+      if(resposta === false) alert("Modelo de Placa Invalido!!!");
+    });
+  return red;
+}
+
+const updateBanco = (dado) => {
+
   for(let i = 0; i < data.length; i++){
     if(data[i].id === dado.id){
       data[i].id = dado.id
@@ -92,5 +115,4 @@ export const update = async (dado) => {
   }
 
   red = true;
-  return red;
 }
